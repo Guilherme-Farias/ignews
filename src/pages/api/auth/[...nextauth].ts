758595f94d51f74ccf68ebfa6ec44280c386a026
Guilherme Farias <guicfarias11@gmail.com>
2021-03-25 12:24:53 -0300

    Feature de salvar usuário no banco finalizada

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+import { query as q } from 'faunadb'
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -9,5 +11,26 @@ export default NextAuth({
       scope: "read:user"
     }),
   ],
-  database: process.env.DATABASE_URL,
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+      try {
+        await fauna.query(
+          q.If(q.Not(q.Exists(q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                ))),
+            q.Create(q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              ))))
+        return true
+      } catch {
+        return false
+      }
+    },
+  },
 })
